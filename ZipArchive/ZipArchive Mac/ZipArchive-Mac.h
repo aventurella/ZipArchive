f29c80c861daeec6dfd630c1e0a0a2bc8c31a650
Adam Venturella <aventurella@blitzagency.com>
2015-09-05 10:53:26 -0700

    adds mac target

--- ZipArchive/ZipArchive Mac/ZipArchive-Mac.h
+++ ZipArchive/ZipArchive Mac/ZipArchive-Mac.h
@@ -1,20 +1,18 @@
 //
-//  ZipArchive.h
-//  ZipArchive
+//  ZipArchive Mac.h
+//  ZipArchive Mac
 //
 //  Created by Adam Venturella on 9/5/15.
 //  Copyright (c) 2015 BLITZ. All rights reserved.
 //
 
+#import <Cocoa/Cocoa.h>
 
-#import <Foundation/Foundation.h>
-
-//! Project version number for ZipArchive.
+//! Project version number for ZipArchive Mac.
 FOUNDATION_EXPORT double ZipArchiveVersionNumber;
 
-//! Project version string for ZipArchive.
+//! Project version string for ZipArchive Mac.
 FOUNDATION_EXPORT const unsigned char ZipArchiveVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <ZipArchive/PublicHeader.h>
 #import <ZipArchive/Archive.h>
-
